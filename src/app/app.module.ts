94a55e3dfb1dda9d4a50700c3215e7cd8462310f
Kevin <kevinfiorentino.1294@gmail.com>
2022-05-29 17:19:30 -0300

    SharedModule

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { SharedModule } from './modules/shared/shared.module';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -13,6 +14,7 @@ import { NotFoundComponent } from './components/not-found/not-found.component';
   imports: [
     BrowserModule,
     AppRoutingModule,
+    SharedModule,
   ],
   exports: [],
   providers: [],
