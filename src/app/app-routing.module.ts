e1be8ca23bd325a174e330c2477c1abb58fc20c4
Kevin <kevinfiorentino.1294@gmail.com>
2022-05-30 16:31:07 -0300

    Precarga de Modulos

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,15 +1,18 @@
 import { NgModule } from '@angular/core';
-import { RouterModule, Routes } from '@angular/router';
+import { RouterModule, Routes, PreloadAllModules } from '@angular/router';
+import { CustomPreloadingStrategyService } from './modules/shared/services/custom-preloading-strategy.service';
 import { NotFoundComponent } from './components/not-found/not-found.component';
 
 const routes: Routes = [
   {
     path: '',
-    loadChildren: () => import('./modules/website/website.module').then(m => m.WebsiteModule)
+    loadChildren: () => import('./modules/website/website.module').then(m => m.WebsiteModule),
+    data: { preload: true },
   },
   {
     path: 'cms',
-    loadChildren: () => import('./modules/cms/cms.module').then(m => m.CmsModule)
+    loadChildren: () => import('./modules/cms/cms.module').then(m => m.CmsModule),
+    data: { preload: true },
   },
   {
     path: '**',
@@ -18,7 +21,10 @@ const routes: Routes = [
 ];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [RouterModule.forRoot(routes, {
+    /* preloadingStrategy: PreloadAllModules, */
+    preloadingStrategy: CustomPreloadingStrategyService,
+  })],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
